dfaa2446ed389f2cc64d7685605ec22b1b0d5a5d
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-01-31 16:40:14 +0200

    Persistence skeleton

---
+++ _db.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ *  Image Class, for the details of a "mititel"
+ *
+ *  @param {String} name     image filename
+ *  @param {String} caption  image caption text
+ *  @example $imageObj = new Img('filename', 'caption text');
+ */
+class Img
+{
+	public $name;
+	public $caption;
+
+	function __construct($name = '', $caption = '') {
+		$this->name = $name;
+		$this->caption = $caption;
+	}
+}
+
+
+$details = [];
+
+$details['uno'] = array(
+	// texts
+	'title' => 'Uno',
+	'textIntro' => 'Dupa uno, vine Doina',
+	'textDescription' => 'Long description, dolor sit amet. Long description, dolor sit amet. Long description, dolor sit amet. Long description, dolor sit amet. Long description, dolor sit amet.',
+
+	// images
+	'imagesFolder' => '01uno',
+	'images' => array(
+		new Img(
+			'uno1.jpg',
+			'Lorem ipsum, dolor sit 1'
+		),
+		new Img(
+			'uno2.jpg',
+			'Lorem ipsum, dolor sit 2'
+		),
+		new Img(
+			'uno3.jpg',
+			'Lorem ipsum, dolor sit 3'
+		),
+		new Img(
+			'uno4.jpg',
+			'Lorem ipsum, dolor sit 4'
+		),
+		new Img(
+			'uno5.jpg',
+			'Lorem ipsum, dolor sit 5'
+		),
+		new Img(
+			'uno6.jpg',
+			'Lorem ipsum, dolor sit 6'
+		)
+	)
+);
+
+// echo '<pre>'; var_dump($details); 
--- details.php
+++ details.php
@@ -1,13 +1,29 @@
 <?php
 	require_once('_config.php');
+	require_once('_db.php');
 
-	$pageTitle = $_GET['item'] . ' - ' . $TITLE;
+	$item = $_GET['item'];
+	$pageTitle = $item . ' - ' . $TITLE;
+	$content = $details[$item];
 
 	require_once('includes/header.php');
 ?>
 
 	<section>
-		<h1><?php echo $_GET['item'];?></h1>
+		<h1><?php echo $content['title'];?></h1>
+		<p><?php echo $content['textIntro'];?></p>
+		<p><?php echo $content['textDescription'];?></p>
+
+		<ul>
+			<?php
+				foreach ($content['images'] as $img) {
+					echo '<li>
+						<img src="assets/images/'.$content['imagesFolder'].'/'.$img->name.'" alt="" />
+						<p>'.$img->caption.'</p>
+					</li>';
+				}
+			?>
+		</ul>
 	</section>
 
 <?php
