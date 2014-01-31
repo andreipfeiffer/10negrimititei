c276c7c58b3f6b35c8bb6a96b4955e5c34ed38f8
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-01-31 14:01:47 +0200

    Initial commit, project skeleton

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+	<title>10 Negri Mititei</title>	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta property="og:title" content="10 Negri Mititei"/>
+	<meta property="og:site_name" content="10 Negri Mititei"/>
+	<meta property="og:url" content="http://10negrimititei.ro"/>
+	<meta property="og:description" content="10 Negri Mititei pleacă în lume să colecţioneze fapte bune."/>
+	<meta name="description" content="" />
+
+	<link rel="stylesheet" href="assets/css/normalize.css" />
+	<link rel="stylesheet" href="assets/css/helpers.css" />
+</head>
+<body>
+
+	<header id="page-header">
+		<h1>LOGO 10 negri mititei</h1>
+		<nav>
+			<h2 class="visuallyhidden">Navigare principala</h2>
+			<ul>
+				<li>echipa</li>
+				<li>contact</li>
+			</ul>
+		</nav>
+	</header>
+
+	<footer id="page-footer">
+
+	</footer>
+
+	<script src="assets/js/libs/modernizr.custom.js"></script>
+	<script src="assets/js/libs/jquery-1.10.1.min.js"></script>
+	<script src="assets/js/libs/response-0.7.8.min.js"></script>
+</body>
+</html>
