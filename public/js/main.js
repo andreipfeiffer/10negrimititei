30278bc003c8271eba9aa72aee4004d753cd4137
Andrei Pfeiffer <andrei@upsidedown.ro>
2014-03-02 17:49:14 +0200

    Responsive typography on title and subtitles

--- public/js/main.js
+++ public/js/main.js
@@ -6,4 +6,11 @@ $(function() {
 		height: 1000
 	});
 
+	$('.b-heading__title').bigtext({
+		maxfontsize: 160 // default is 48 (in px)
+	});
+	// $('.b-heading__subtitle').bigtext({
+	// 	maxfontsize: 32 // default is 48 (in px)
+	// });
+
 });
